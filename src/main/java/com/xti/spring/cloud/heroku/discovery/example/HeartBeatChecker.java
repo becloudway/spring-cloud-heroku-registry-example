466ec0aeadc833934c4961608e01b9ce0b2bf1b0
Michael Willemse <michael.willemse@xt-i.com>
2017-04-12 21:43:31 +0200

    temporarily added heartbeat checker

---
+++ src/main/java/com/xti/spring/cloud/heroku/discovery/example/HeartBeatChecker.java
@@ -0,0 +1,15 @@
+package com.xti.spring.cloud.heroku.discovery.example;
+
+import org.springframework.cloud.client.discovery.event.HeartbeatEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HeartBeatChecker {
+
+    @EventListener
+    @SuppressWarnings("UnusedParameters")
+    public void updateMemberships(HeartbeatEvent event) {
+        System.out.println("heartbeat");
+    }
+}
