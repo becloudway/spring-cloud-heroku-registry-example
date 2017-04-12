cde52878018a91673e40757d0b224cd173a33bd9
Michael Willemse <michael.willemse@xt-i.com>
2017-04-12 21:22:32 +0200

    add spring cloud discovery topology endpoint

--- src/main/java/com/xti/spring/cloud/heroku/discovery/example/TopologyController.java
+++ src/main/java/com/xti/spring/cloud/heroku/discovery/example/TopologyController.java
@@ -1,5 +1,8 @@
 package com.xti.spring.cloud.heroku.discovery.example;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,12 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/topology")
 public class TopologyController {
 
-    @RequestMapping(method = RequestMethod.GET)
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @RequestMapping(path = "/file", method = RequestMethod.GET)
     public String getSpaceTopology(){
         try {
             return new String(Files.readAllBytes(Paths.get("/etc/heroku/space-topology.json")));
@@ -21,5 +28,20 @@ public class TopologyController {
             return "failed to get space topology";
         }
 
+    }
+
+    @RequestMapping(path = "/springcloud", method = RequestMethod.GET)
+    public String getSpringCloudTopology(){
+        StringBuilder sb = new StringBuilder();
+        List<String> services = discoveryClient.getServices();
+        for (String service : services) {
+            sb.append(service).append("\n");
+            List<ServiceInstance> instances = discoveryClient.getInstances(service);
+            for (ServiceInstance instance : instances) {
+                sb.append(instance.getHost()).append("\n");
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
     }
 }
