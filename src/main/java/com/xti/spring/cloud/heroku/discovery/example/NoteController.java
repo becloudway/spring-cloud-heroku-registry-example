69a41ddcdf4f731c88eb9e686ba9462f7aba91f3
Michael Willemse <michael.willemse@xt-i.com>
2017-04-13 17:29:46 +0200

    fixed incorrect handling noteId path variable

--- src/main/java/com/xti/spring/cloud/heroku/discovery/example/NoteController.java
+++ src/main/java/com/xti/spring/cloud/heroku/discovery/example/NoteController.java
@@ -2,12 +2,12 @@ package com.xti.spring.cloud.heroku.discovery.example;
 
 import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.websocket.server.PathParam;
 import java.util.UUID;
 
 @RestController
@@ -29,8 +29,8 @@ public class NoteController {
         return new CreateNoteResponse(createNoteRequest.getCount());
     }
 
-    @RequestMapping(path = "/{noteId}", method = RequestMethod.POST)
-    public UpdateNoteResponse updateNoteRequest(@PathParam("noteId") String noteId, @RequestBody UpdateNoteRequest updateNoteRequest){
+    @RequestMapping(value = "/{noteId}", method = RequestMethod.POST)
+    public UpdateNoteResponse updateNoteRequest(@PathVariable("noteId") String noteId, @RequestBody UpdateNoteRequest updateNoteRequest){
 
         commandGateway.sendAndWait(new UpdateNoteCommand(noteId, updateNoteRequest.getText()));
 
